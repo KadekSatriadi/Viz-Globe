2079747cdff6cfad22fb52ba699506ee7b562b02
Kadek Satriadi <ananta.satriadi@gmail.com>
2021-09-24 16:12:53 +1000

    Restructured to Unity project

---
+++ Assets/Viz-Globe/Scripts/Data/Database.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Database : MonoBehaviour
+{
+    protected List<Dictionary<string, object>> data;
+
+
+    public abstract List<Dictionary<string, object>> GetRecordsByField(string fieldname);
+    public abstract List<float> GetFloatRecordsByField(string fieldname, string con);
+
+    public abstract List<string> GetStringRecordsByField(string fieldname, string con);
+
+    public abstract List<object> GetUniqueValues(string fieldname);
+
+    public virtual List<Dictionary<string, object>> GetData()
+    {
+        return data;
+    }
+
+}
